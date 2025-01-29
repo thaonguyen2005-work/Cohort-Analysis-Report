db02f293d1534ac76aeef584894f12e442882516
Thao Nguyen <thaonguyenth2005@gmail.com>
2025-01-28 19:25:02 -0500

    Update Cohort_analysis.sql

--- Cohort_analysis.sql
+++ Cohort_analysis.sql
@@ -108,28 +108,6 @@ from product_profit
 ) as rank_table
 Where rank_table.rank<=5
 order by rank_table.month_year
-Select * from 
-(With product_profit as
-(
-Select 
-CAST(FORMAT_DATE('%Y-%m', t1.delivered_at) AS STRING) as month_year,
-t1.product_id as product_id,
-t2.name as product_name,
-round(sum(t1.sale_price),2) as sales,
-round(sum(t2.cost),2) as cost,
-round(sum(t1.sale_price)-sum(t2.cost),2)  as profit
-from bigquery-public-data.thelook_ecommerce.order_items as t1
-Join bigquery-public-data.thelook_ecommerce.products as t2 on t1.product_id=t2.id
-Where t1.status='Complete'
-Group by month_year, t1.product_id, t2.name
-)
-Select * ,
-dense_rank() OVER ( PARTITION BY month_year ORDER BY month_year,profit) as rank
-from product_profit
-) as rank_table
-Where rank_table.rank<=5
-order by rank_table.month_year
-
 --5-- Revenue to date for each category Statistics of total daily revenue for each product category in the past 3 months (assuming the current date is April 15, 2022).
 Select 
 CAST(FORMAT_DATE('%Y-%m-%d', t1.delivered_at) AS STRING) as dates,
